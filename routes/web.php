dd5afdc0e497428121e0b41f71e8ac2fb37645e6
prabodha <prabodhasenadhipathi123@gmail.com>
2020-08-16 17:04:51 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->group(['prefix' => 'students'], function () use ($router) {
+    $router->get('/', 'StudentController@showAllStudents');
+    $router->get('/{id}', 'StudentController@showOneStudents');
+    $router->post('/', 'StudentController@create');
+    $router->put('/{id}', 'StudentController@update');
+    $router->delete('/{id}', 'StudentController@delete');
+    });
