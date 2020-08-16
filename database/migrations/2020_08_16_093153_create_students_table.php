4ea5bd377ec3f8f66dbe562b4f809be2b1f20715
prabodha <prabodhasenadhipathi123@gmail.com>
2020-08-16 15:34:03 +0530

    initial project

---
+++ database/migrations/2020_08_16_093153_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->longText('address');
+            $table->date('birthday');
+            $table->string('gender');
+            $table->integer('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
